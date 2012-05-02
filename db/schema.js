73e48bc4cd8fd422fd436d01fc564e726c42fb47
Johan Öbrink <johan.obrink@24hr.se>
2012-05-02 10:36:07 +0200

    railway setup

---
+++ db/schema.js
@@ -0,0 +1,29 @@
+/*
+ db/schema.js contains database schema description for application models
+ by default (when using jugglingdb as ORM) this file uses database connection
+ described in config/database.json. But it's possible to use another database
+ connections and multiple different schemas, docs available at
+
+ http://railwayjs.com/orm.html
+
+ Example of model definition:
+
+ define('User', function () {
+     property('email', String, { index: true });
+     property('password', String);
+     property('activated', Boolean, {default: false});
+ });
+
+ Example of schema configured without config/database.json (heroku redistogo addon):
+ schema('redis', {url: process.env.REDISTOGO_URL}, function () {
+     // model definitions here
+ });
+
+*/
+var Drone = describe('Drone', function () {
+    property('name', String, { index: true });
+    property('description', String);
+    property('created', Date, { default: Date.now });
+    property('connectionKey', String);
+    property('connected', Boolean, { default: false });
+});
