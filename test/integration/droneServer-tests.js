37dfdddca831b17bd54ef68b00e9542bb497e5cb
Johan Öbrink <johan.obrink@24hr.se>
2012-07-06 17:15:19 +0200

    Drones and Users set up Time to start connecting

--- test/integration/droneServer-tests.js
+++ test/integration/droneServer-tests.js
@@ -1,21 +1,67 @@
 var should = require('should'),
 	io = require('socket.io'),
 	ioClient = require('socket.io-client'),
-	_ = require('underscore');
+	_ = require('underscore'),
+	mongoose = require('mongoose'),
+	async = require('async'),
+
+	Drone = require('../../lib/drone').Drone,
+	User = require('../../lib/user').User;
 
 var port = 5000,
 	host;
 
+var server, drone, user;
+
+// Connect db
+mongoose.connect('mongodb://localhost/dronehub-test');
+
+// Utility functions
+var addDrone = function(cb) {
+	drone = new Drone();
+	drone.name = 'Panda boat';
+	drone.save(function(err) {
+		cb();
+	});
+};
+var removeDrones = function(cb) {
+	Drone.remove(function(err) {
+		cb();
+	});
+};
+
+var addUser = function(cb) {
+	user = new User();
+	user.name = 'Andy';
+	user.save(function(err) {
+		cb();
+	});
+};
+var removeUsers = function(cb) {
+	User.remove(function(err) {
+		cb();
+	});
+};
+
 describe('DroneServer', function() {
 
-	var server;
+	before(function(done) {
+
+		async.parallel([ addDrone, addUser ], done);
+
+	});
+
+	after(function(done) {
+
+		async.parallel([ removeDrones, removeUsers ], done);
+
+	});
 
 	beforeEach(function() {
 
 		var socket = io.listen(++port, { 'log level': 1 });
 		server = require('../../lib/droneServer').create(socket);
 		host = 'http://localhost:' + port;
-
 	});
 
 	afterEach(function() {
@@ -61,16 +107,68 @@ describe('DroneServer', function() {
 
 	});
 
-	it('should register drones', function(done) {
+	it('should connect Drone with correct id', function(done) {
 		_.size(server.drones).should.equal(0);
 
 		var client = ioClient.connect(host);
 		client.on('connect', function() {
 			_.size(server.clients).should.equal(1);
 
-			client.emit('register_drone', 'abc-123', function(err) {
+			client.emit('connect_drone', drone._id, function(err) {
+				should.not.exist(err);
 				_.size(server.clients).should.equal(1);
 				_.size(server.drones).should.equal(1);
+				done();
+			});
+
+		});
+	});
+
+	it('should return an error when trying to connect Drone with incorrect id', function(done) {
+		_.size(server.drones).should.equal(0);
+
+		var client = ioClient.connect(host);
+		client.on('connect', function() {
+			_.size(server.clients).should.equal(1);
+
+			client.emit('connect_drone', 'ed-209', function(err) {
+				should.exist(err);
+				_.size(server.clients).should.equal(1);
+				_.size(server.drones).should.equal(0);
+				done();
+			});
+
+		});
+	});
+
+	it('should connect User with correct id', function(done) {
+		_.size(server.users).should.equal(0);
+
+		var client = ioClient.connect(host);
+		client.on('connect', function() {
+			_.size(server.clients).should.equal(1);
+
+			client.emit('connect_user', user._id, function(err) {
+				should.not.exist(err);
+				_.size(server.clients).should.equal(1);
+				_.size(server.users).should.equal(1);
+				done();
+			});
+
+		});
+	});
+
+	it('should return an error when trying to connect User with incorrect id', function(done) {
+		_.size(server.users).should.equal(0);
+
+		var client = ioClient.connect(host);
+		client.on('connect', function() {
+			_.size(server.clients).should.equal(1);
+
+			client.emit('connect_user', 'cohagen', function(err) {
+				should.exist(err);
+				_.size(server.clients).should.equal(1);
+				_.size(server.users).should.equal(0);
 				done();
 			});
 
